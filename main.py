4d875ff865ddc24ad13c20f181a86cfd3d2b2f63
Robin Kearney <robin@kearney.co.uk>
2018-04-01 21:32:44 +0100

    Experiementing with memcaching the responses

--- main.py
+++ main.py
@@ -2,11 +2,17 @@
 
 import boto3
 from flask import Flask, jsonify, send_from_directory
+import memcache
 import logging
+
+image_list_cache_time = 300
+repo_list_cache_time  = 600
 
 app = Flask(__name__, static_url_path='/')
 
 client = boto3.client('ecr')
+
+mc = memcache.Client(['127.0.0.1:11211'], debug=0)
 
 
 @app.route('/api/v1.0/<string:registry_id>/repositories', methods=['GET'])
@@ -32,30 +38,53 @@ def root():
 
 def get_repositories(registry_id):
 
-    paginator = client.get_paginator('describe_repositories')
-    page_iterator = paginator.paginate(registryId=registry_id)
+    json_response = mc.get('repositories')
 
-    response = {'repositories': []}
+    if not json_response:
+        app.logger.info("cache miss repositories")
+        paginator = client.get_paginator('describe_repositories')
+        page_iterator = paginator.paginate(registryId=registry_id)
 
-    for page in page_iterator:
-        response['repositories'].extend(page['repositories'])
+        response = {'repositories': []}
 
+        for page in page_iterator:
+            response['repositories'].extend(page['repositories'])
 
-    return jsonify(response)
+        json_response = jsonify(response)
+
+        mc.set('repositories', json_response, repo_list_cache_time)
+    else:
+        app.logger.info("cache hit repositories")
+
+    return json_response
 
 
 def list_repository(registry_id, repository_name):
 
-    paginator = client.get_paginator('describe_images')
-    page_iterator = paginator.paginate(
-        registryId=registry_id, repositoryName=repository_name)
+    cache_key = "{0}-{1}".format(registry_id, repository_name)
 
-    response = {'imageDetails': []}
+    json_response = mc.get(cache_key)
 
-    for page in page_iterator:
-        response['imageDetails'].extend(page['imageDetails'])
+    if not json_response:
+        app.logger.info("cache miss {0}".format(cache_key))
 
-    return jsonify(response)
+        paginator = client.get_paginator('describe_images')
+        page_iterator = paginator.paginate(
+            registryId=registry_id, repositoryName=repository_name)
+
+        response = {'imageDetails': []}
+
+        for page in page_iterator:
+            response['imageDetails'].extend(page['imageDetails'])
+
+        json_response = jsonify(response)
+
+        mc.set(cache_key, json_response, image_list_cache_time)
+
+    else:
+        app.logger.info("cache hit {0}".format(cache_key))
+
+    return json_response
 
 
 if __name__ == '__main__':
